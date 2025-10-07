58b62a8aef6b5e888219aba5e965b666eff2362d
Fabrizrod <alcantarafabrizio3@gmail.com>
2025-10-07 12:04:19 -0500

    Redirección desde / hacia /vehiculos/

--- gestion/urls.py
+++ gestion/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vehiculos.urls')),
+    path('vehiculos/', include('vehiculos.urls')),
+    path('', RedirectView.as_view(url='/vehiculos/')),  # Redirige / hacia /vehiculos/
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
