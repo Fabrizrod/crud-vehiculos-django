619d729f546f2e409bc9233fc6d19d6e9f8fcbc7
Fabrizrod <alcantarafabrizio3@gmail.com>
2025-10-07 12:18:05 -0500

    Agregado menú y rutas para propietarios

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Vehículos
     path('', views.lista_vehiculos, name='lista_vehiculos'),
     path('crear/', views.crear_vehiculo, name='crear_vehiculo'),
     path('editar/<int:id>/', views.editar_vehiculo, name='editar_vehiculo'),
     path('eliminar/<int:id>/', views.eliminar_vehiculo, name='eliminar_vehiculo'),
 
+    # Propietarios
     path('propietarios/', views.lista_propietarios, name='lista_propietarios'),
     path('propietarios/crear/', views.crear_propietario, name='crear_propietario'),
     path('propietarios/editar/<int:id>/', views.editar_propietario, name='editar_propietario'),
     path('propietarios/eliminar/<int:id>/', views.eliminar_propietario, name='eliminar_propietario'),
 ]
-
