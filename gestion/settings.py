5bf9be893309a63a47164345fc112912504f9174
Fabrizrod <alcantarafabrizio3@gmail.com>
2025-10-07 11:58:10 -0500

    Cambio a SQLite para despliegue

--- gestion/settings.py
+++ gestion/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion_vehiculos',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
