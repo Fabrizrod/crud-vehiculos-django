0d141812bb02e9979366ed22e0e58345b35d310c
Fabrizrod <alcantarafabrizio3@gmail.com>
2025-10-07 12:32:24 -0500

    Configuración final de media y static para Render

--- gestion/urls.py
+++ gestion/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vehiculos/', include('vehiculos.urls')),
-    path('', RedirectView.as_view(url='/vehiculos/')),  # Redirige / hacia /vehiculos/
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
